0665d80c9a43ea57df9d0a8b402359a88fd1d24c
pricnenishad904 <princenishad904@gmail.com>
2024-03-22 09:36:22 +0530

    add app

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -6,11 +6,7 @@ const PlayerInfo = ({ team }) => {
       <div className="w-12  h-12 overflow-hidden rounded-full">
         <img src={team.image} alt="" />
       </div>
-      <div
-        className={`w-[70px] px-1 bg-black ${
-          team.whereTeam != team.whereTeam ? "bg-white" : "bg-black"
-        } text-white h-auto rounded-sm`}
-      >
+      <div className={`w-[70px] px-1 bg-black text-white h-auto rounded-sm`}>
         <div className="relative flex flex-col justify-center">
           <h2 className={`truncate text-center text-[12px] `}>{team?.name}</h2>
           <span className="absolute top-4 left-4 text-center text-[12px] uppercase">
